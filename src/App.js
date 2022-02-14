932af4c3bd36d9ad3ed80a5e8f70914591ed204e
mayinx <mayinxx@web.de>
2022-02-14 16:39:16 +0100

    Add base layout structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>COVID-19-Dashboard</h1>
       </header>
+      <main className="main">CONTENT</main>
+      <footer className="footer">FOOTER</footer>
     </div>
   );
 }
